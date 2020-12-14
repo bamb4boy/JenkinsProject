d78785c4d7e388051c5b81f3420d83c31e744406
bamb4boy <glebgamegg@gmail.com>
2020-12-14 19:19:29 +0000

    added permissions to test file

--- Jenkins/Jenkinsfile.groovy
+++ Jenkins/Jenkinsfile.groovy
@@ -21,7 +21,7 @@ pipeline {
                 dir('coronadata/tests') {
                     script{
                         try {
-                            sh "./basic.test.sh"
+                            sh "sudo ./basic.test.sh"
                         } catch (err) {
                             println("Error thrown on test file execution")
                             currentBuild.result = 'ABORTED'
