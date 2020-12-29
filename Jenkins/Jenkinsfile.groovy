0c8e7d4c4c9964f592ec7009be6dc1507b907347
bamb4boy <glebgamegg@gmail.com>
2020-12-29 07:14:16 +0000

    added docker hub script and deployment

--- Jenkins/Jenkinsfile.groovy
+++ Jenkins/Jenkinsfile.groovy
@@ -11,7 +11,14 @@ pipeline {
             steps {
                 dir('coronadata'){
                     script {
-                        sh "sudo docker build -t coronadata ."
+                        println("Getting commit id and latest Version")
+			_lastCommit = sh script: "git log | head -1 | awk '{print \$2}' | cut -c1-6", returnStdout: true
+			_latestVersion = sh script: "git branch -r | cut -d '/' -f2 | grep 0. | sort -r | head -1", returnStdout: true
+			lastCommit = _lastCommit.trim()
+			latestVersion = _latestVersion.trim()
+			println("Latest Version seen is ${latestVersion}")
+			println("Latest commit seen is ${lastCommit}")
+			sh "sudo docker build -t bamb4boy/coronadata:${latestVersion}-${lastCommit} . "
                     }
                 }
             }
@@ -33,9 +40,17 @@ pipeline {
         }
         stage('Upload image to repository') {
             steps {
-                sh "pwd"
+                sh "sudo docker push bamb4boy/coronadata:${latestVersion}-${lastCommit}"
             }
         } 
-        
+        stage('Deploy to Prod') {
+	    steps {
+	       script {
+	           dir('deployment') {
+		       sh "ansible-playbook -i inventory.ini coronadata.yml --extra-vars tag=${latest version}-${lastCommit}
+                   }
+               }
+            }
+        }     
     }
 }
