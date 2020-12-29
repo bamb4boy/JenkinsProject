b2391c5dc49a48122d361c0dfbebe76d4935972f
bamb4boy <glebgamegg@gmail.com>
2020-12-29 16:09:08 +0000

    pls

--- Jenkins/Jenkinsfile.groovy
+++ Jenkins/Jenkinsfile.groovy
@@ -48,7 +48,7 @@ pipeline {
 	    steps {
 	       script {
 	           dir('deployment') {
-		       sh '/usr/bin/ansible-playbook -i inventory.ini coronadata.yml --extra-vars tag=\${latestVersion}-\${lastCommit}'
+		       sh '/usr/bin/ansible-playbook -i inventory.ini coronadata.yml --extra-vars tag=\$latestVersion-\$lastCommit'
                    }
                }
 
