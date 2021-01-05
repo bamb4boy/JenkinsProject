fa36c69a7d2f32f7d8f34c744cdbadc0b01d0584
root <root@DESKTOP-VQTHU2G.localdomain>
2021-01-05 20:01:59 +0200

    adding new directory with flask code basic

---
+++ Flask Code/Flask_app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+#imporing the flask repository
+app = Flask(__name__)
+#configuring a variable named the same as my file name
+@app.route("/hello", methods=["GET"])
+#defining the app route or url
+def hello():
+    return "Hello from Flask!"
+#defining function
+app.run()
+#leaves the app running until i quit it
